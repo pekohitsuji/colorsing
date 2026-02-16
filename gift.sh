67ef9881f9e5df0b200ecb1247b225d19d2ecc2e
pekohitsuji <kaeru0921@icloud.com>
2026-02-17 08:46:37 +0900

    [Change] Pages 公開のため (md => html 変換なし)

--- gift.sh
+++ gift.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # -*- coding: utf-8-unix; mode: shell-script -*-
+
 gosh ./gift-list '70 コイン以上' img/gift/?????-???.png > gift-list.md
 gosh ./gift-list 'ハート限定' img/gift/{00255-001,00288-001,00321-001,00399-001,01111-001,01214-001,01222-003,03939-001}.png > gift-list-heart.md
+
+pandoc -f gfm -t html gift-list.md       -o gift-list.html
+pandoc -f gfm -t html gift-list-heart.md -o gift-list-heart.html
+
+pandoc -f gfm -t html README.md | sed 's/\.md"/.html"/g' > index.html
