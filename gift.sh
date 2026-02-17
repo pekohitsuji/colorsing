28ab78445bed41547182bb172b1bbb990e532dab
pekohitsuji <kaeru0921@icloud.com>
2026-02-17 11:24:11 +0900

    [Add] CSS

--- gift.sh
+++ gift.sh
@@ -4,7 +4,12 @@
 gosh ./gift-list '70 コイン以上' img/gift/?????-???.png > gift-list.md
 gosh ./gift-list 'ハート限定' img/gift/{00255-001,00288-001,00321-001,00399-001,01111-001,01214-001,01222-003,03939-001}.png > gift-list-heart.md
 
-pandoc -f gfm -t html gift-list.md       -o gift-list.html
-pandoc -f gfm -t html gift-list-heart.md -o gift-list-heart.html
+function trans() {
+    pandoc -f gfm -t html \
+           -s --css css/base.css --metadata title="${1}" ${@:2}
+}
 
-pandoc -f gfm -t html README.md | sed 's/\.md"/.html"/g' > index.html
+trans "70コイン以上" gift-list.md       -o gift-list.html
+trans "ハート限定"   gift-list-heart.md -o gift-list-heart.html
+
+trans 'ColorSing あれこれ'  README.md | sed 's/\.md"/.html"/g' > index.html
